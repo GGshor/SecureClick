e52c66e9c4c9f1db6aad6f1a71aa3101e8fbafff
GGshor <73340248+GGshor@users.noreply.github.com>
2022-10-30 20:00:59 +0100

    expanded demo

--- demo/SecureInputDemo.server.lua
+++ demo/SecureInputDemo.server.lua
@@ -6,4 +6,15 @@ local input = SecureInput.new(workspace.SpawnLocation):AddInput("ClickDetector")
 
 input:ConnectActivated(function(player)
 	print(player.Name, "has activated input!")
+	input:Disable()
+	task.wait(10)
+	input:Enable()
+	
+	input:SetMaxActivationDistance(10)
+	print("Max activation distance set to 10")
+	
+	task.wait(10)
+	
+	input:Timeout(player, 10)
+	print("Timed out", player.Name, "for 10 seconds")
 end)
